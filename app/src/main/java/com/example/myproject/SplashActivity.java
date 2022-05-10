f61959bbe6432d7dc96592d547571a0d23718f66
qwezzet <MaximDLM@yandex.ru>
2022-05-10 20:17:55 +0700

    SplashActivity.java

--- app/src/main/java/com/example/myproject/SplashActivity.java
+++ app/src/main/java/com/example/myproject/SplashActivity.java
@@ -1,14 +1,38 @@
 package com.example.myproject;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(1111);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                }
+            }
+        };
+        thread.start();
     }
 }
