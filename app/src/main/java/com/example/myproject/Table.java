136aa50813af1992774a11f31c16632fab372176
qwezzet <MaximDLM@yandex.ru>
2022-05-19 23:06:43 +0700

    cleaning (right)

--- app/src/main/java/com/example/myproject/Table.java
+++ app/src/main/java/com/example/myproject/Table.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Table extends AppCompatActivity implements View.OnClickListener {
     Button backtomain, aboutsizes;
+
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,79 +32,49 @@ public class Table extends AppCompatActivity implements View.OnClickListener {
             backtomain.setTextColor(Color.GRAY);
         });
 
-            aboutsizes = findViewById(R.id.btnaboutsizes);
-            aboutsizes.setOnClickListener(v -> {
-                Intent intent = new Intent(Table.this, Description.class);
-                startActivity(intent);
-                aboutsizes.setTextColor(Color.GRAY);
-            });
+        aboutsizes = findViewById(R.id.btnaboutsizes);
+        aboutsizes.setOnClickListener(v -> {
+            Intent intent = new Intent(Table.this, Description.class);
+            startActivity(intent);
+            aboutsizes.setTextColor(Color.GRAY);
+        });
 
 
     }
-
-    public void showAlertDialogInform1(View v){
+    private void alertDialogs(int i2, int i3, int i4, int lengthShort) {
         String[] array = getResources().getStringArray(R.array.mainstates);
         String[] arraydesc = getResources().getStringArray(R.array.aboutsizesintable);
-        AlertDialog.Builder alert = new AlertDialog.Builder( this);
-        alert.setTitle(array[0]);
-        alert.setMessage(arraydesc[0]);
-        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this,array[3], Toast.LENGTH_SHORT).show());
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle(array[i2]);
+        alert.setMessage(arraydesc[i3]);
+        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this, array[i4], lengthShort).show());
         alert.create().show();
     }
 
-
-
-    public void showAlertDialogInform2(View v){
-        String[] array = getResources().getStringArray(R.array.mainstates);
-        String[] arraydesc = getResources().getStringArray(R.array.aboutsizesintable);
-        AlertDialog.Builder alert = new AlertDialog.Builder( this);
-        alert.setTitle(array[0]);
-        alert.setMessage(arraydesc[1]);
-        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this,array[3], Toast.LENGTH_SHORT).show());
-        alert.create().show();
+    public void showAlertDialogInform1(View v) {
+        alertDialogs(0, 0, 3, Toast.LENGTH_SHORT);
     }
 
 
-
-
-
-    public void showAlertDialogInform3(View v){
-        String[] array = getResources().getStringArray(R.array.mainstates);
-        String[] arraydesc = getResources().getStringArray(R.array.aboutsizesintable);
-        AlertDialog.Builder alert = new AlertDialog.Builder( this);
-        alert.setTitle(array[0]);
-        alert.setMessage(arraydesc[2]);
-        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this,array[3], Toast.LENGTH_SHORT).show());
-        alert.create().show();
+    public void showAlertDialogInform2(View v) {
+        alertDialogs(0, 1, 3, Toast.LENGTH_SHORT);
     }
 
 
-
-
-
-    public void showAlertDialogInform4(View v){
-        String[] array = getResources().getStringArray(R.array.mainstates);
-        String[] arraydesc = getResources().getStringArray(R.array.aboutsizesintable);
-        AlertDialog.Builder alert = new AlertDialog.Builder( this);
-        setTitle(array[0]);
-        alert.setMessage(arraydesc[3]);
-        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this,array[3], Toast.LENGTH_SHORT).show());
-        alert.create().show();
+    public void showAlertDialogInform3(View v) {
+        alertDialogs(0, 2, 3, Toast.LENGTH_SHORT);
     }
 
 
-
-
-
-    public void showAlertDialogDesk(View v){
-        String[] array = getResources().getStringArray(R.array.mainstates);
-        String[] arraydesc = getResources().getStringArray(R.array.aboutsizesintable);
-        AlertDialog.Builder alert = new AlertDialog.Builder( this);
-        alert.setTitle(array[1]);
-        alert.setMessage(arraydesc[4]);
-        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this,array[4], Toast.LENGTH_LONG).show());
-        alert.create().show();
+    public void showAlertDialogInform4(View v) {
+        alertDialogs(0, 3, 3, Toast.LENGTH_SHORT);
     }
+
+
+    public void showAlertDialogDesk(View v) {
+        alertDialogs(1, 4, 4, Toast.LENGTH_LONG);
+    }
+
     public void onClick(View v) {
     }
 }
