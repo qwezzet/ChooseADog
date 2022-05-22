b9b89a3377878d565a333e19c35a9dc8bb4c22e1
qwezzet <MaximDLM@yandex.ru>
2022-05-22 17:00:30 +0700

    lastest update (maybe no)

--- app/src/main/java/com/example/myproject/Table.java
+++ app/src/main/java/com/example/myproject/Table.java
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,14 +15,16 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Table extends AppCompatActivity implements View.OnClickListener {
-    Button backtomain, aboutsizes;
+public class Table extends AppCompatActivity  {
+    Button backtomain, aboutsizes, btnAlertTablAll, btnFrAlertTabl;
+
 
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_tabl);
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
@@ -38,43 +42,61 @@ public class Table extends AppCompatActivity implements View.OnClickListener {
             startActivity(intent);
             aboutsizes.setTextColor(Color.GRAY);
         });
-
-
+        btnAlertTablAll = findViewById(R.id.btniform);
+        btnFrAlertTabl = findViewById(R.id.abcBtn);
+        btnAlertTablAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogGetTabl();
+            }
+        });
     }
-    private void alertDialogs(int i2, int i3, int i4, int lengthShort) {
+
+
+    public void dialogGetTabl() {
+        String[] arrayTabl = getResources().getStringArray(R.array.mainarray);
+        AlertDialog.Builder builderTabl = new AlertDialog.Builder(this);
+        View viewtab = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dialog_layout_tabl, findViewById(R.id.dialLayTable));
+        builderTabl.setView(viewtab);
+        AlertDialog alertDialogTabl = builderTabl.create();
+        alertDialogTabl.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialogTabl.setCancelable(false);
+        btnFrAlertTabl = viewtab.findViewById(R.id.abcBtn);
+        btnFrAlertTabl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialogTabl.cancel();
+                Toast.makeText(Table.this, arrayTabl[0], Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertDialogTabl.show();
+    }
+    private void alertDialogs(int i3) {
         String[] array = getResources().getStringArray(R.array.mainstates);
         String[] arraydesc = getResources().getStringArray(R.array.aboutsizesintable);
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle(array[i2]);
+        alert.setTitle(array[0]);
         alert.setMessage(arraydesc[i3]);
-        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this, array[i4], lengthShort).show());
+        alert.setPositiveButton(array[2], (dialogInterface, i) -> Toast.makeText(Table.this, array[3], Toast.LENGTH_SHORT).show());
         alert.create().show();
     }
 
     public void showAlertDialogInform1(View v) {
-        alertDialogs(0, 0, 3, Toast.LENGTH_SHORT);
+        alertDialogs(0);
     }
 
 
     public void showAlertDialogInform2(View v) {
-        alertDialogs(0, 1, 3, Toast.LENGTH_SHORT);
+        alertDialogs(1);
     }
 
 
     public void showAlertDialogInform3(View v) {
-        alertDialogs(0, 2, 3, Toast.LENGTH_SHORT);
+        alertDialogs(2);
     }
 
 
     public void showAlertDialogInform4(View v) {
-        alertDialogs(0, 3, 3, Toast.LENGTH_SHORT);
-    }
-
-
-    public void showAlertDialogDesk(View v) {
-        alertDialogs(1, 4, 4, Toast.LENGTH_LONG);
-    }
-
-    public void onClick(View v) {
+        alertDialogs(3);
     }
 }
--- app/src/main/java/com/example/myproject/TwoActivity.java
+++ app/src/main/java/com/example/myproject/TwoActivity.java
@@ -1,7 +1,6 @@
 package com.example.myproject;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.drawable.Drawable;
