cd66286fcf8b3541a0ea2578c2381c48329b238f
qwezzet <MaximDLM@yandex.ru>
2022-05-26 21:03:09 +0700

    problem with filters + add navigation

--- app/src/main/java/com/example/myproject/Description.java
+++ app/src/main/java/com/example/myproject/Description.java
@@ -6,15 +6,21 @@ import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Description extends AppCompatActivity {
     Button back;
+
     @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +40,39 @@ public class Description extends AppCompatActivity {
         mediaController.setAnchorView(videoView);
 
 
+        BottomNavigationView bottomNavi = findViewById(R.id.bottom_navi);
+        bottomNavi.setOnNavigationItemSelectedListener(navListener);
+
+
         back = findViewById(R.id.btnfromvideototable);
         back.setOnClickListener(v -> {
-            Intent i = new Intent(Description.this,Table.class);
+            Intent i = new Intent(Description.this, Table.class);
             startActivity(i);
             back.setTextColor(Color.GRAY);
         });
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.first_step:
+                    selectedFragment = new StepFragment_1();
+                    break;
+                case R.id.second_step:
+                    selectedFragment = new StepFragment_2();
+                    break;
+                case R.id.third_step:
+                    selectedFragment = new StepFragment_3();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_cont,selectedFragment).commit();
+            return true;
+        }
+    };
+
 }
